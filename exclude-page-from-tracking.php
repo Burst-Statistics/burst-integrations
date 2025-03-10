26bf6a6f045402e94ad8f76b630736a56ec04e9d
Rogier Lankhorst <rogierlankhorst@gmail.com>
2025-03-10 10:53:33 +0100

    remove trailingslashit

--- exclude-page-from-tracking.php
+++ exclude-page-from-tracking.php
@@ -9,11 +9,13 @@
  */
 
 function set_referrer_to_spammer( $sanitized_data ) {
-    $url = 	trailingslashit( $sanitized_data['host']) . $sanitized_data['page_url'];
+    $url = 	$sanitized_data['host'] . $sanitized_data['page_url'];
     if ( str_contains($url, 'my-page-url') ){
         //we use a trick, by setting the referrer to spammer, this page won't get tracked.
         $sanitized_data['referrer'] = 'spammer';
     }
+    
+
 
     return $sanitized_data;
 }
